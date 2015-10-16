5cce35e780acb3c495f4b9aa4a305e6911741f80
HenryLoenwind <henry@loenwind.info>
2015-10-16 10:35:56 +0200

    Added special handling for The Drain being on fire
    depending on its content

--- src/main/java/info/loenwind/enderioaddons/machine/drain/BlockDrain.java
+++ src/main/java/info/loenwind/enderioaddons/machine/drain/BlockDrain.java
@@ -4,6 +4,7 @@ import info.loenwind.enderioaddons.EnderIOAddons;
 import info.loenwind.enderioaddons.common.GuiIds;
 import info.loenwind.enderioaddons.config.Config;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,6 +13,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -22,10 +24,14 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import com.enderio.core.api.client.gui.IAdvancedTooltipProvider;
 import com.enderio.core.client.handlers.SpecialTooltipHandler;
+import com.enderio.core.common.TileEntityEnder;
+import com.enderio.core.common.util.BlockCoord;
 import com.enderio.core.common.util.FluidUtil;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -38,6 +44,7 @@ import crazypants.enderio.machine.AbstractMachineBlock;
 import crazypants.enderio.machine.AbstractMachineEntity;
 import crazypants.enderio.machine.power.PowerDisplayUtil;
 import crazypants.enderio.network.PacketHandler;
+import crazypants.enderio.tool.SmartTank;
 
 public class BlockDrain extends AbstractMachineBlock<TileDrain> implements IAdvancedTooltipProvider {
 
@@ -255,4 +262,65 @@ public class BlockDrain extends AbstractMachineBlock<TileDrain> implements IAdva
   @Override
   public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
   }
+
+  @Override
+  public boolean isFireSource(World world, int x, int y, int z, ForgeDirection side) {
+    if (side == ForgeDirection.UP) {
+      TileEntityEnder te = getTileEntityEio(world, x, y, z);
+      if (te instanceof TileDrain) {
+        final SmartTank tank = ((TileDrain) te).tank;
+        if (tank.getFluidAmount() > 0) {
+          FluidStack fluidStack = tank.getFluid();
+          if (fluidStack != null && fluidStack.getFluid() != null) {
+            if (fluidStack.getFluid() == EnderIO.fluidHootch) {
+              tank.addFluidAmount(-5);
+              ((TileDrain) te).setTanksDirty();
+              return true;
+            } else if (fluidStack.getFluid() == EnderIO.fluidFireWater) {
+              tank.addFluidAmount(-2);
+              ((TileDrain) te).setTanksDirty();
+              return true;
+            } else if (fluidStack.getFluid() == EnderIO.fluidRocketFuel) {
+              tank.setFluid(null);
+              world.setBlockToAir(x, y, z);
+              world.newExplosion(null, x + .5f, y + 1.5f, z + .5f, 3, true, true);
+              world.newExplosion(null, x + .5f, y + 3f, z + .5f, 2, true, true);
+              world.newExplosion(null, x + .5f, y + 4.5f, z + .5f, 1, true, true);
+              world.newExplosion(null, x + .5f, y + 6f, z + .5f, 1, true, true);
+              return true;
+            } else if (fluidStack.getFluid() == FluidRegistry.WATER) {
+              for (BlockCoord bc1 : getAround(te.getLocation())) {
+                if (bc1.getBlock(world) == Blocks.fire) {
+                  world.setBlockToAir(bc1.x, bc1.y, bc1.z);
+                  world.playSoundEffect(bc1.x + 0.5F, bc1.y + 0.1F, bc1.z + 0.5F, "random.fizz", 0.5F,
+                      2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+                  tank.addFluidAmount(-20);
+                }
+              }
+              ((TileDrain) te).setTanksDirty();
+              return false;
+            } else if (fluidStack.getFluid() == FluidRegistry.LAVA) {
+              return world.rand.nextInt(5) != 0;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private static List<BlockCoord> getAround(BlockCoord bc0) {
+    List<BlockCoord> result = new ArrayList<>();
+    for (int x = -1; x <= 1; x++) {
+      for (int y = -1; y <= 1; y++) {
+        for (int z = -1; z <= 1; z++) {
+          if (x != 0 || y != 0 || z != 0) {
+            result.add(new BlockCoord(bc0.x + x, bc0.y + y, bc0.z + z));
+          }
+        }
+      }
+    }
+    return result;
+  }
+
 }
